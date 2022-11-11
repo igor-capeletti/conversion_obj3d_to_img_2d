608b7987c2524399adbf45020bd847c06ec4a523
igor-capeletti <igorcapeletti.aluno@unipampa.edu.br>
2022-11-11 17:09:23 -0300

    adicionando imagens geradas

--- script_execucao.sh
+++ script_execucao.sh
@@ -2,27 +2,29 @@
 echo "Algoritmo converter um objeto 3D em uma imagem normal: --------------------------------------"
 echo ""
 
-g++ -o exec cria_imagens_3D_POO.cpp -std=c++11
+g++ -o exec cria_imagens_3D_POO.cpp -std=c++11      #compilacao do codigo em C++
 
-#objetos 3D para escolha
+
+#------------------- variaveis para escolha personalizada -------------------
+#objetos 3D para escolha ---
 arq_origem="cara.obj"
 #arq_origem="weird.obj"
 
-
+#arquivos que serao gerados pela execucao ---
 arq_buffer="buffer.obj"
 arq_destino="imagem_gerada.ppm"
 
-#angulos em graus
+#angulos em graus ---
 rotacao_x="20"
 rotacao_y="130"
 rotacao_z="0"
 
-#ponto central da imagem 2D que vai entrar a imagem 3D
+#ponto (X,Y) central da imagem 2D que vai entrar o objeto 3D ---
 centro_x="300"
 centro_y="300"
 
 #escala(escala do objeto 3D na imagem 2D)
-escala="230"
+escala="230"              
 
 #cor do objeto 3D
 red_frente="255"
@@ -35,6 +37,7 @@ green_fundo="0"
 blue_fundo="255"
 
 
+#------------------- normalizacao dos arquivos -------------------
 echo "Normalização do obejto 3D:"
 ctmconv $arq_origem $arq_buffer --no-normals --no-texcoords
 arq_origem="sufixo.obj"
@@ -42,7 +45,11 @@ arq_origem="sufixo.obj"
 #script em python para retirar barras dos arquivos
 python3 limpa_lixo_arquivo_obj.py $arq_buffer $arq_origem
 
+
+#------------------- execucao do algoritmo -------------------
 ./exec $arq_origem $arq_destino $rotacao_x $rotacao_y $rotacao_z $centro_x $centro_y $escala $red_frente $green_frente $blue_frente $red_fundo $green_fundo $blue_fundo
 
+
+#exclusao de arquivos -------
 rm $arq_buffer
 rm exec
